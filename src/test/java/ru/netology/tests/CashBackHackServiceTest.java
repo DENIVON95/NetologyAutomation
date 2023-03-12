f327b46793d9e219f85d417937cd48b4d70247c0
DENIVON95 <ivonin.denis@mail.ru>
2023-03-12 22:06:09 +0400

    added junit4 tests

--- src/test/java/ru/netology/tests/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/tests/CashBackHackServiceTest.java
@@ -1,34 +1,33 @@
 package ru.netology.tests;
 
-
-import org.testng.Assert;
+import org.junit.Assert;
 import ru.netology.service.CashBackHackService;
 
 public class CashBackHackServiceTest {
 
     private CashBackHackService cashBackHackService = new CashBackHackService();
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void checkWithinBoundaryAmount() {
         int testAmount = 900;
         int expectedAmount = 100;
         int remainCashback = cashBackHackService.remain(testAmount);
-        Assert.assertEquals(remainCashback, expectedAmount);
+        Assert.assertEquals(expectedAmount, remainCashback);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void checkCashBackBoundaryAmount() {
         int testAmount = 1000;
         int expectedAmount = 0;
         int remainCashback = cashBackHackService.remain(testAmount);
-        Assert.assertEquals(remainCashback, expectedAmount);
+        Assert.assertEquals(expectedAmount, remainCashback);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void checkCashBackWithZeroAmount() {
         int testAmount = 0;
         int expectedAmount = 1000;
         int remainCashback = cashBackHackService.remain(testAmount);
-        Assert.assertEquals(remainCashback, expectedAmount);
+        Assert.assertEquals(expectedAmount, remainCashback);
     }
 }
